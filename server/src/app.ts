3384207d6a8e50801e4f12fbc9bee5ce17485666
Maxim <toper.one111@gmail.com>
2024-02-09 12:31:43 +0200

    feat: update server limit upload

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,8 +25,8 @@ app.use(compression());
 app.use(cors());
 app.options('*', cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ extended: true, limit: '200mb' }));
 
 app.use('/', mainController);
 
