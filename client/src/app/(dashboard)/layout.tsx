10c6162a287b6c46aa528291f403d55b7161345b
Maxim <toper.one111@gmail.com>
2024-02-10 03:03:25 +0200

    fix: access auctions without login

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,12 +1,24 @@
 "use client";
 
 import { Header } from "@/components/layout/header";
+import { useAuth } from "@/providers/auth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export type AuctionsProps = {
   children: React.ReactNode;
 };
 
 export default function Auctions({ children }: AuctionsProps) {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/sign-in");
+    }
+  }, [user, router]);
+
   return (
     <>
       <Header />
